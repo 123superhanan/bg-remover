7bc5615a65753a80d5a88831292167bee8d04b88
Abdul Hanan Abbasi <146124897+123superhanan@users.noreply.github.com>
2024-11-13 15:12:11 +0500

    project-cereation

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import BuyCredit from './Pages/BuyCredit'
+import Result from './Pages/Result'
+import Home from './Pages/HOME'
+import Navbar from './Components/Navbar'
+import Footer from './Components/Footer'
+
+const App = () => {
+
+
+  
+  return (
+    <>
+    <Navbar/>
+    <Routes>
+
+    <Route path="/" element={<Home />} />
+    <Route path="/Result" element={<Result/>} />
+    <Route path="/Buy" element={ <BuyCredit/>} />
+
+
+    </Routes>
+<Footer/>
+
+
+    </>
+  )
+}
+
+export default App
