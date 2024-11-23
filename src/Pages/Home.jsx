213125e1237976feb03d530977479a70bdd95faf
Abdul Hanan Abbasi <146124897+123superhanan@users.noreply.github.com>
2024-11-23 15:54:18 +0500

    bg-slider-components

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Header from '../Components/Header'
 import Steps from '../Components/Steps'
+import BgSlide from '../Components/bgSlide'
+import Testimonial from '../Components/Testimonial'
 
 const Home = () => {
   return (
     <div>
      <Header />
      <Steps />
+     <BgSlide/>
+     <Testimonial/>
     </div>
   )
 }
